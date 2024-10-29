c73e726c9a6e7e557c421cf42b7e7a71fc8531cb
imdanaa <jangs1920@gmail.com>
2024-10-29 12:13:15 +0900

    init

---
+++ src/main/java/cloud/woong/apiserver/FrontController.java
@@ -0,0 +1,28 @@
+package cloud.woong.apiserver;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class FrontController {
+    @GetMapping("/")
+    public Map<String, Object> index() {
+        Map<String, Object> data = new HashMap<>();
+        data.put("result", "success");
+
+        List<Map> list = new ArrayList<>();
+        Map<String, String> map1 = new HashMap<>();
+        map1.put("id", "woong");
+        map1.put("name", "윤정");
+        list.add(map1);
+
+        data.put("list", list);
+
+        return data;
+    }
+}
